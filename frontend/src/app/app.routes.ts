0a5087858f70631e0c596d4e18de7fb237714a1a
Gerardo Duarte <gaduarter@ufpso.edu.co>
2025-03-15 12:01:27 -0500

    Pagina home y carrito agregadas

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegistroComponent } from './registro/registro.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CartComponent } from './pages/cart/cart.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'home',
+        title: 'home',
+        component: HomeComponent
+    },
+    {
+        path: 'login',
+        title: 'login',
+        component: LoginComponent
+    },
+    {
+        path: 'register',
+        title: 'register',
+        component: RegistroComponent
+    },
+    {
+        path: 'cart',
+        title: 'cart',
+        component: CartComponent
+    },
+    
+];
